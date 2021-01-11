c8e1ee07047a6d905dd712d7b65214b051376d79
MrGuzior <konrad.guzior@gmail.com>
2021-01-11 13:02:44 +0100

    Initial refactor

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Counter } from '../../features/counter/Counter'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  )
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { store } from './app/store';
+import App from './Components/App/App';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './service/serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
