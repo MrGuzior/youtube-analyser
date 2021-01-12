b671e4ae32a5a58affea1e01bbd965f17c8c49e5
MrGuzior <konrad.guzior@gmail.com>
2021-01-12 13:00:38 +0100

    Changing strategy, from html to json

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { getSubscriptionsNames, getAllSubscriptionsInfo, getHistory } from '../../analyser/analyser.js'
+import { getSubscriptionsNames, getAllSubscriptionsInfo } from '../../analyser/analyser.js'
 
 function App() {
-  const historyList = getHistory()
 
+  console.log(getSubscriptionsNames())
   return (
     <div>
-      {historyList.map(item => <p>{item.video}</p>)}
+      <p>lol</p>
     </div>
   )
 }
